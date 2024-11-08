daf280891d051e315318d048e6e60a791e514882
Lucas Murback <159513098+LucMurbs@users.noreply.github.com>
2024-11-08 14:25:15 -0300

    Update Dados brutos.py

--- pages/Dados brutos.py
+++ pages/Dados brutos.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time 
 
 
-@st.cache_data #essa função manter o arquivo armazenado no cache do app, caso o Df não seja filtrado ou modificado ele não precisa ficar convertendo toda vez, ele já vai ter salvo
+@st.cache_data 
 def converte_csv(df):
     return df.to_csv(index = False).encode('utf-8')
 
@@ -18,8 +18,8 @@ st.title('DADOS BRUTOS')
     
 url = 'https://labdados.com/produtos'
 
-response = requests.get(url) #Usei o params para passar o filtro que eu fiz usando a própria url
-dados = pd.DataFrame.from_dict(response.json()) #aqui transforei o json que foi carregado na requisição url, para um DataFrame
+response = requests.get(url) 
+dados = pd.DataFrame.from_dict(response.json()) 
 dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format = '%d/%m/%Y')
 
 
@@ -28,19 +28,19 @@ with st.expander('Colunas'):
 
 st.sidebar.title('Filtros')
 with st.sidebar.expander('Nome Do Produto'):
-    produtos = st.multiselect('Selecione Os Produtos', dados['Produto'].unique(), dados['Produto'].unique()) #multiselect para colunas com dados categóricos
+    produtos = st.multiselect('Selecione Os Produtos', dados['Produto'].unique(), dados['Produto'].unique()) 
 
 with st.sidebar.expander('Categoria Do Produto'):
     categoria = st.multiselect('Selecione As Categorias', dados['Categoria do Produto'].unique(), dados['Categoria do Produto'].unique())
 
 with st.sidebar.expander('Preço Do Produto'):
-    preco = st.slider('Selecione o Preço', 0, 5000, (0, 5000)) #slider Para colunas numéricas
+    preco = st.slider('Selecione o Preço', 0, 5000, (0, 5000)) 
 
 with st.sidebar.expander('Freta Da Venda'):
     frete = st.slider('Frete', 0, 250, (0, 250)) 
 
 with st.sidebar.expander('Data Da Compra'):
-    data_compra = st.date_input('Selecione a Data', (dados['Data da Compra'].min(), dados['Data da Compra'].max())) #date_input para datas
+    data_compra = st.date_input('Selecione a Data', (dados['Data da Compra'].min(), dados['Data da Compra'].max())) 
 
 with st.sidebar.expander('Vendedor'):
     vendedores = st.multiselect('Selecione Os Vendedores', dados['Vendedor'].unique(), dados['Vendedor'].unique())
@@ -49,8 +49,7 @@ with st.sidebar.expander('Local Da Compra'):
     local_compra = st.multiselect('Selecione o Local Da Compra', dados['Local da compra'].unique(), dados['Local da compra'].unique())
 
 with st.sidebar.expander('Avaliação Da Compra'):
-    avaliacao = st.slider('Avaliação', dados['Avaliação da compra'].min(), dados['Avaliação da compra'].max(), (dados['Avaliação da compra'].min(), dados['Avaliação da compra'].max())) #aqui eu fiz de um jeito diferente para mostrar que é possível!!
-
+    avaliacao = st.slider('Avaliação', dados['Avaliação da compra'].min(), dados['Avaliação da compra'].max(), (dados['Avaliação da compra'].min(), dados['Avaliação da compra'].max())) 
 with st.sidebar.expander('Tipo De Pagamento'):
     tipo_pagamento = st.multiselect('Selecione o Tipo De Pagamento', dados['Tipo de pagamento'].unique(), dados['Tipo de pagamento'].unique())
 
